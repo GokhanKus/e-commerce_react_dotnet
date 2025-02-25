c83cc41ed0a629f2d861586fc73dadfed6c10eec
Gokhan Kus <gkus1998@hotmail.com>
2025-02-25 10:44:28 +0300

    creating redux store (Provider store)

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,9 +3,13 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import { router } from './router/Router.tsx'
 import CartContextProvider from './context/CartContext.tsx'
+import { Provider } from 'react-redux'
+import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <CartContextProvider>
-    <RouterProvider router={router} />
-  </CartContextProvider>
+  <Provider store={store}>
+    <CartContextProvider>
+      <RouterProvider router={router} />
+    </CartContextProvider>
+  </Provider>
 )
