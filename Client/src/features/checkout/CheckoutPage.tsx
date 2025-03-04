74d23b15ccc3e75860498634eae48d3e45bc469a
Gokhan Kus <gkus1998@hotmail.com>
2025-03-04 16:17:47 +0300

    checkout page, info page

--- Client/src/features/checkout/CheckoutPage.tsx
+++ Client/src/features/checkout/CheckoutPage.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { Paper, Grid2 } from "@mui/material";
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import Review from "./Review";
+import Info from "./Info";
 
 function CheckoutPage() {
+
     return (
-        <div>
-            cehckouts
-        </div>
-    )
+        <Paper>
+            <Grid2 container sx={{ p: 4 }} spacing={5}>
+                <Grid2 size={4}>
+                    <Info />
+                </Grid2>
+                <Grid2 size={8}>
+                    <AddressForm />
+                    <PaymentForm />
+                    <Review />
+                </Grid2>
+            </Grid2>
+        </Paper>
+    );
+
 }
 
 export default CheckoutPage
